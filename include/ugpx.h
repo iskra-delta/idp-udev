b87af22ad55532720036a36cade37a42ae16735e
tstih <tomaz.stih@post.com>
2022-07-17 13:41:22 +0100

    Removed faulty functions.

--- include/ugpx.h
+++ include/ugpx.h
@@ -52,10 +52,13 @@ extern void gputpixel(coord x, coord y);
 extern void gputglyph(void* glyph, coord x, coord y);
 
 /* return LINE or TINY glyph dimensions */
-extern void gmeasureglyph(void *glyph, coord *x, coord *y);
+extern void gmeglyph(void *glyph, coord *w, coord *h);
 
 /* print string */
 extern void gputtext(void *font, char *text, coord x, coord y);
+
+/* get text size */
+extern void gmetext(void *font, char *text, coord *w, coord *h);
 
 /* set color, sets drawing color 
    NOTES: 
